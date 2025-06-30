758fc0e5315a1842aa1e195dce72b7da7bce1f89
javelosa <49219594+javelosa@users.noreply.github.com>
2025-06-30 12:24:58 +0800

    Updated JS city and current temp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function renewData(response) {
+  let temperatureElement = document.querySelector("#weather-temp");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "04a6b97ba438oct66060d748685ff445";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(renewData);
+}
+
 function goSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", goSearchSubmit);
+
+searchCity("Barcelona");
